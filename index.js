5a5df4b7a49b62211b0e66c07b069df0a857b358
PaulSamPS <psamoylenko2@gmail.com>
2022-05-02 03:21:36 +0500

    подготовка проекта

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const cors = require('cors')
+const router = require('./routes/index')
+const path = require('path')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+
+const PORT = process.env.PORT || 4000
+
+const app = express()
+app.use(cookieParser(process.env.SECRET_COOKIE))
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+)
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
