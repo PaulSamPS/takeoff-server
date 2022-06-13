f2d82e2c877f03c6d34f60a2b05e2f089c5ac61c
PaulSamPS <psamoylenko2@gmail.com>
2022-06-13 10:07:36 +0500

    добавил модели для постов и подписчииков

---
+++ models/followers.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose')
+
+const FollowerSchema = new Schema(
+    {
+        user: { type: Schema.Types.ObjectId, ref: 'User' },
+
+        followers: [
+            {
+                user: { type: Schema.Types.ObjectId, ref: 'User' }
+            }
+        ],
+
+        following: [
+            {
+                user: { type: Schema.Types.ObjectId, ref: 'User' }
+            }
+        ]
+    }
+)
+
+module.exports = model('Follower', FollowerSchema)
