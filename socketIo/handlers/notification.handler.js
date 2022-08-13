14cf9d42e0f18fb38002597309a72437321663bc
Pavel Samoylenko <psamoylenko2@gmail.com>
2022-08-13 21:38:01 +0300

    переименовал сокет оповещений

--- socketIo/handlers/notification.handler.js
+++ socketIo/handlers/notification.handler.js
@@ -3,15 +3,20 @@ const User = require('../../models/user.model')
 module.exports = function notificationHandler(io, socket) {
   socket.on('notification:get', async ({ userId }) => {
     const notification = await getAllNotifications(userId)
-    const user = await User.findById(userId)
-    socket.emit('notifications:count', { count: user.notificationCount })
     socket.emit('notifications', { notification })
+  })
+
+  socket.on('notifications:countGet', async ({ userId }) => {
+    const user = await User.findById(userId)
+    socket.emit('notifications:countSuccess', { count: user.notificationCount })
   })
 
   socket.on('notifications:read', async ({ userId, readNotificationsCount }) => {
     const user = await User.findById(userId)
-    user.notificationCount = user.notificationCount - readNotificationsCount
-    await user.save()
+    if (user) {
+      user.notificationCount = user.notificationCount - readNotificationsCount
+      await user.save()
+    }
     socket.emit('notifications:unread', { count: user.notificationCount })
   })
 }
